d7f80bf7a7880b932c9e358ec66f47aac236fddc
Gabriel Patron <gchase.patron@gmail.com>
2017-01-07 12:55:49 +0800

    Make todo models, setup and connect db to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -118,6 +119,10 @@ app.put('/todos/:id', (req, res) => {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, () => {
-	console.log(`Express listeninng on port ${PORT}`)
-});
+db.sequelize.sync()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Express listening on port ${PORT}`)
+		});
+	});
+
